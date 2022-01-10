6be3159d2d87235538411e33db800f624325be3e
SmileXie <smilexie1113@gmail.com>
2022-01-10 15:33:16 +0800

    enqueue a placeholder when last node dequeues

--- lock_free_queue.c
+++ lock_free_queue.c
@@ -11,6 +11,7 @@
 #define LFQ_LOG_ERROR printf
 #define LFQ_LOG_INFO printf
 
+#define PLACEHOLDER_DATA 0xee
 
 int lf_queue_init(queue_t *queue)
 {
@@ -30,7 +31,7 @@ int lf_queue_init(queue_t *queue)
 
     memset(init_node, 0, sizeof(struct lf_queue_node));
     init_node->next = NULL;
-    init_node->data[0] = 111;
+    memset(init_node->data, PLACEHOLDER_DATA, sizeof(init_node->data));
 
     head_init.aba = 0;
     head_init.node = init_node;
@@ -82,10 +83,40 @@ int lf_queue_enqueue(queue_t *queue, char *data, size_t data_len)
     return 0;
 }
 
+/* enqueue a empty data node to push-out previous node */
+static int lf_queue_enqueue_placeholder_data(queue_t *queue)
+{
+    char data[LF_QUEUE_DATA_LEN];
+    int ret;
+
+    memset(data, PLACEHOLDER_DATA, sizeof(data));
+
+    ret = lf_queue_enqueue(queue, data, LF_QUEUE_DATA_LEN);
+    if (ret != 0) {
+        LFQ_LOG_ERROR("fail to equeue empty data.\n");
+        return -1;
+    }
+
+    LFQ_LOG_INFO("enqueue placeholder node.\n");
+
+    return 0;
+}
+
+bool lf_queue_is_placeholder_node(char *data, int len)
+{
+    char ph_data[LF_QUEUE_DATA_LEN];
+    int ret;
+
+    memset(ph_data, PLACEHOLDER_DATA, sizeof(data));
+
+    return memcmp(ph_data, data, len) == 0;
+
+}
+
 int lf_queue_dequeue(queue_t *queue, char *data, size_t data_len)
 {
     struct lf_queue_head new_head, origin_head;
-    bool empty;
+    int ret;
 
     if (queue == NULL) {
         LFQ_LOG_ERROR("queue is NULL\n");
@@ -97,26 +128,33 @@ int lf_queue_dequeue(queue_t *queue, char *data, size_t data_len)
         return -1;
     }
 
-    empty = false;
     do {
         origin_head = atomic_load(&queue->head);
-        if (origin_head.node->next == NULL) {
-            /* never remove the last node */
-            LFQ_LOG_INFO("empty queue\n");
-            empty = true;
-            break;
+        while (origin_head.node->next == NULL) {
+            LFQ_LOG_INFO("last node, enqueue an empty node to push-out the node.\n");
+            /* always maintain one node in queue, enqueue a placeholder */
+            ret = lf_queue_enqueue_placeholder_data(queue);
+            if (ret != 0) {
+                LFQ_LOG_ERROR("fail to equeue empty data.\n");
+                return -1;
+            }
+            origin_head = atomic_load(&queue->head);
         }
 
         new_head.node = origin_head.node->next;
         new_head.aba = origin_head.aba + 1;
     } while(!atomic_compare_exchange_weak(&queue->head, &origin_head, new_head));
 
-    if (!empty) {
-        memcpy(data, origin_head.node->data, data_len);
-        free(origin_head.node);
-        LFQ_LOG_INFO("dequeue data: %d %d %d %d %d %d, aba: %"PRIuPTR"\n", 
-            data[0], data[1], data[2], data[3], data[4], data[5], origin_head.aba);
+    memcpy(data, origin_head.node->data, data_len);
+    free(origin_head.node);
+
+    while (lf_queue_is_placeholder_node(data, data_len)) {
+        (void)lf_queue_dequeue(queue, data, data_len);
     }
+
+    LFQ_LOG_INFO("dequeue data: %d %d %d %d %d %d, aba: %"PRIuPTR"\n", 
+        data[0], data[1], data[2], data[3], data[4], data[5], origin_head.aba);
+
 
     return 0;
 }
